9cfa0192067737f92954808706ee907308df07a9
1F10240198 <s1f102401988@iniad.org>
2026-04-17 17:00:24 +0900

    custom userとmodelsを作成

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_public = models.BooleanField(default=False)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Seasoning(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=50)
+
+class RecipeSeasoning(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.Case)
+    Seasoning = models.ForeignKey(Seasoning,  on_delete=models.CASCADE)
+    amount = models.CharField(max_length=50)
